ec0809b1080581d276ef4c6036700dc3630fd29a
Amanpreet Kaur <amankaur2628@gmail.com>
2020-04-14 16:44:24 -0300

    [Aman]LoginActivity: Binding done

--- app/src/main/java/com/example/c0782918_w2020_mad3125_fp/ui/LoginActivity.java
+++ app/src/main/java/com/example/c0782918_w2020_mad3125_fp/ui/LoginActivity.java
@@ -1,16 +1,41 @@
 package com.example.c0782918_w2020_mad3125_fp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.CompoundButton;
 
 import com.example.c0782918_w2020_mad3125_fp.R;
+import com.example.c0782918_w2020_mad3125_fp.databinding.ActivityMainBinding;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 public class LoginActivity extends AppCompatActivity {
 
+    ActivityMainBinding mainBinding;
+
+  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+
+
+
+
     }
+
+
 }
