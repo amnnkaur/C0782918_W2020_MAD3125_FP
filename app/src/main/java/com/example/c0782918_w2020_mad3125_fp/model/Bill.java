bb4ac9bd13fbb02fd6a64683b4026a2453b80045
Amanpreet Kaur <amankaur2628@gmail.com>
2020-04-14 20:11:07 -0300

    [Aman]Bill : Class created with variables, constructor & getter-setter

---
+++ app/src/main/java/com/example/c0782918_w2020_mad3125_fp/model/Bill.java
@@ -0,0 +1,51 @@
+package com.example.c0782918_w2020_mad3125_fp.model;
+
+import java.util.Date;
+
+public abstract class Bill {
+
+
+    String billID;
+    BillType billType;
+    Date billDate;
+    Double billTotal;
+
+    public Bill(String billID, BillType billType, Date billDate, Double billTotal) {
+        this.billID = billID;
+        this.billType = billType;
+        this.billDate = billDate;
+        this.billTotal = billTotal;
+    }
+
+    public String getBillID() {
+        return billID;
+    }
+
+    public void setBillID(String billID) {
+        this.billID = billID;
+    }
+
+    public BillType getBillType() {
+        return billType;
+    }
+
+    public void setBillType(BillType billType) {
+        this.billType = billType;
+    }
+
+    public Date getBillDate() {
+        return billDate;
+    }
+
+    public void setBillDate(Date billDate) {
+        this.billDate = billDate;
+    }
+
+    public Double getBillTotal() {
+        return billTotal;
+    }
+
+    public void setBillTotal(Double billTotal) {
+        this.billTotal = billTotal;
+    }
+}
---
+++ app/src/main/java/com/example/c0782918_w2020_mad3125_fp/model/BillType.java
@@ -0,0 +1,6 @@
+package com.example.c0782918_w2020_mad3125_fp.model;
+
+public enum BillType {
+
+    MOBILE, INTERNET, HYDRO
+}
