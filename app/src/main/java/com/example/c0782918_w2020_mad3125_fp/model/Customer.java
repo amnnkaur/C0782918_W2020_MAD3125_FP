782706dfa3a577715724a2a6c5bcf19173669340
Amanpreet Kaur <amankaur2628@gmail.com>
2020-04-14 20:01:32 -0300

    [Aman]Customer: Constructor created

--- app/src/main/java/com/example/c0782918_w2020_mad3125_fp/model/Customer.java
+++ app/src/main/java/com/example/c0782918_w2020_mad3125_fp/model/Customer.java
@@ -11,7 +11,20 @@ public class Customer {
     private String gender;
     private String birthDate;
 
-  
+    public Customer() {
+    }
+
+    public Customer(int customerID, String firstName, String lastName, String fullName, String email, String phoneNumber, String gender, String birthDate) {
+        this.customerID = customerID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.fullName = fullName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.gender = gender;
+        this.birthDate = birthDate;
+    }
+
 
 }
 
