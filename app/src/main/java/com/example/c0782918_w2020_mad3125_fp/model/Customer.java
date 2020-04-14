1bd8dbe13449eff155178a0558dc31a23d87fb36
Amanpreet Kaur <amankaur2628@gmail.com>
2020-04-14 20:00:47 -0300

    [Aman]Customer: Variables declared

--- app/src/main/java/com/example/c0782918_w2020_mad3125_fp/model/Customer.java
+++ app/src/main/java/com/example/c0782918_w2020_mad3125_fp/model/Customer.java
@@ -1,4 +1,17 @@
 package com.example.c0782918_w2020_mad3125_fp.model;
 
 public class Customer {
+
+    private int customerID;
+    private String firstName;
+    private String lastName;
+    private String fullName;
+    private String email;
+    private String phoneNumber;
+    private String gender;
+    private String birthDate;
+
+  
+
 }
+
