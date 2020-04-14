f7f0ba3ec3603ff4b7b6e4c5b1f975e08ed1031e
Amanpreet Kaur <amankaur2628@gmail.com>
2020-04-14 16:45:21 -0300

    [Aman]usersLogin: json file added

--- app/src/main/java/com/example/c0782918_w2020_mad3125_fp/ui/LoginActivity.java
+++ app/src/main/java/com/example/c0782918_w2020_mad3125_fp/ui/LoginActivity.java
@@ -23,7 +23,7 @@ public class LoginActivity extends AppCompatActivity {
 
     ActivityMainBinding mainBinding;
 
-  
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
