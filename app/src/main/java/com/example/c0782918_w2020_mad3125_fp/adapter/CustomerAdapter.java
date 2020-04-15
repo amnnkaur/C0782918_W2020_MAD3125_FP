2c4c7b655349a920ec805461120d45fceb4c7605
Amanpreet Kaur <amankaur2628@gmail.com>
2020-04-15 15:41:30 -0300

    [Aman]CustomerAdapter: methods implemented

--- app/src/main/java/com/example/c0782918_w2020_mad3125_fp/adapter/CustomerAdapter.java
+++ app/src/main/java/com/example/c0782918_w2020_mad3125_fp/adapter/CustomerAdapter.java
@@ -1,5 +1,42 @@
 package com.example.c0782918_w2020_mad3125_fp.adapter;
 
-public class CustomerAdapter {
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.c0782918_w2020_mad3125_fp.model.Customer;
+
+import java.util.ArrayList;
+
+public class CustomerAdapter extends RecyclerView.Adapter<CustomerAdapter.CustomerViewHolder> {
+
+    private ArrayList<Customer> customerArrayList;
+
+    public CustomerAdapter(ArrayList<Customer> customerArrayList) {
+        this.customerArrayList = customerArrayList;
+    }
+
+    @NonNull
+    @Override
+    public CustomerViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CustomerViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.customerArrayList.size();
+    }
+
+    public class CustomerViewHolder extends RecyclerView.ViewHolder {
+        public CustomerViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
