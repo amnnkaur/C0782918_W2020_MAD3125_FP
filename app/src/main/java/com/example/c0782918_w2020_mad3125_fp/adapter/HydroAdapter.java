b564d0265898d4f0203964dd0d51062426f3141e
Amanpreet Kaur <amankaur2628@gmail.com>
2020-04-16 16:04:10 -0300

    [Aman]HydroAdapter created

---
+++ app/src/main/java/com/example/c0782918_w2020_mad3125_fp/adapter/HydroAdapter.java
@@ -0,0 +1,31 @@
+package com.example.c0782918_w2020_mad3125_fp.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HydroAdapter extends RecyclerView.Adapter<HydroAdapter.HydroViewHolder> {
+    @NonNull
+    @Override
+    public HydroViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HydroViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class HydroViewHolder extends RecyclerView.ViewHolder {
+        public HydroViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
