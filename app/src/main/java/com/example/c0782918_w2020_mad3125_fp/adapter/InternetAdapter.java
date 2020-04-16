da1cf33168babed0704234bd7bc343584658a998
Amanpreet Kaur <amankaur2628@gmail.com>
2020-04-16 16:05:29 -0300

    [Aman]InternetAdapter created

---
+++ app/src/main/java/com/example/c0782918_w2020_mad3125_fp/adapter/InternetAdapter.java
@@ -0,0 +1,31 @@
+package com.example.c0782918_w2020_mad3125_fp.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class InternetAdapter  extends RecyclerView.Adapter<InternetAdapter.InternetViewHolder> {
+    @NonNull
+    @Override
+    public InternetViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull InternetViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class InternetViewHolder extends RecyclerView.ViewHolder {
+        public InternetViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
