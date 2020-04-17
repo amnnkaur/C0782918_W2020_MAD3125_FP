7402eea113c4e6fbfc33842dbe87f4b6a23fdaf7
Amanpreet Kaur <amankaur2628@gmail.com>
2020-04-16 22:00:44 -0300

    [Aman]MobileAdapter completed

--- app/src/main/java/com/example/c0782918_w2020_mad3125_fp/adapter/MobileAdapter.java
+++ app/src/main/java/com/example/c0782918_w2020_mad3125_fp/adapter/MobileAdapter.java
@@ -1,31 +1,60 @@
 package com.example.c0782918_w2020_mad3125_fp.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.c0782918_w2020_mad3125_fp.R;
+import com.example.c0782918_w2020_mad3125_fp.databinding.ItemMobileBinding;
+import com.example.c0782918_w2020_mad3125_fp.model.Customer;
+import com.example.c0782918_w2020_mad3125_fp.model.Mobile;
+
+import java.util.ArrayList;
+
 public class MobileAdapter extends RecyclerView.Adapter<MobileAdapter.MobileViewHolder> {
+
+    private ArrayList<Mobile> mobileArrayList;
+
+    public MobileAdapter(ArrayList<Mobile> mobileArrayList) {
+        this.mobileArrayList = mobileArrayList;
+    }
+
     @NonNull
     @Override
     public MobileViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+
+        ItemMobileBinding itemMobileBinding = DataBindingUtil.inflate(
+                LayoutInflater.from(parent.getContext()), R.layout.item_mobile,parent,false);
+
+        MobileViewHolder mobileViewHolder = new MobileViewHolder(itemMobileBinding);
+        return mobileViewHolder;
+
     }
 
     @Override
     public void onBindViewHolder(@NonNull MobileViewHolder holder, int position) {
 
+        Mobile mobile = this.mobileArrayList.get(position);
+        holder.itemMobileBinding.setMobile(mobile);
+
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return this.mobileArrayList.size();
     }
 
     public class MobileViewHolder extends RecyclerView.ViewHolder {
-        public MobileViewHolder(@NonNull View itemView) {
-            super(itemView);
+
+        ItemMobileBinding itemMobileBinding;
+        public MobileViewHolder(@NonNull ItemMobileBinding itemView) {
+            super(itemView.getRoot());
         }
     }
 }
