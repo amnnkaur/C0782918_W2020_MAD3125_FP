1422e3121ee4d4f752b3b56ba80c7d48b1b8c7ca
Amanpreet Kaur <amankaur2628@gmail.com>
2020-04-16 21:59:36 -0300

    [Aman]InternetAdapter completed

--- app/src/main/java/com/example/c0782918_w2020_mad3125_fp/adapter/InternetAdapter.java
+++ app/src/main/java/com/example/c0782918_w2020_mad3125_fp/adapter/InternetAdapter.java
@@ -1,31 +1,59 @@
 package com.example.c0782918_w2020_mad3125_fp.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.c0782918_w2020_mad3125_fp.R;
+import com.example.c0782918_w2020_mad3125_fp.databinding.ItemInternetBinding;
+import com.example.c0782918_w2020_mad3125_fp.model.Internet;
+
+import java.util.ArrayList;
+
 public class InternetAdapter  extends RecyclerView.Adapter<InternetAdapter.InternetViewHolder> {
+
+    private ArrayList<Internet> internetArrayList;
+
+    public InternetAdapter(ArrayList<Internet> internetArrayList) {
+        this.internetArrayList = internetArrayList;
+    }
+
+
     @NonNull
     @Override
     public InternetViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+
+
+        ItemInternetBinding itemInternetBinding = DataBindingUtil.inflate(
+                LayoutInflater.from(parent.getContext()), R.layout.item_internet,parent,false);
+
+        InternetViewHolder internetViewHolder = new InternetViewHolder(itemInternetBinding);
+        return internetViewHolder;
+
     }
 
     @Override
     public void onBindViewHolder(@NonNull InternetViewHolder holder, int position) {
 
+        Internet internet = this.internetArrayList.get(position);
+        holder.itemInternetBinding.setInternet(internet);
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return this.internetArrayList.size();
     }
 
     public class InternetViewHolder extends RecyclerView.ViewHolder {
-        public InternetViewHolder(@NonNull View itemView) {
-            super(itemView);
+
+        ItemInternetBinding itemInternetBinding;
+        public InternetViewHolder(@NonNull ItemInternetBinding itemView) {
+            super(itemView.getRoot());
         }
     }
 }
