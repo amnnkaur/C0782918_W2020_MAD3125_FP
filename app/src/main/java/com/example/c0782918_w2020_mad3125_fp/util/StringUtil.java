32f21a5eb959c4291b7d1ba70348534f25d2d9f6
Amanpreet Kaur <amankaur2628@gmail.com>
2020-04-16 22:01:39 -0300

    [Aman]convertDateTostring() created

--- app/src/main/java/com/example/c0782918_w2020_mad3125_fp/util/StringUtil.java
+++ app/src/main/java/com/example/c0782918_w2020_mad3125_fp/util/StringUtil.java
@@ -1,6 +1,9 @@
 package com.example.c0782918_w2020_mad3125_fp.util;
 
+import java.text.DateFormat;
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class StringUtil {
 
@@ -12,6 +15,12 @@ public class StringUtil {
         DecimalFormat df = new DecimalFormat("#.##");
 
         return ("$" + String.valueOf(df.format(value)));
+    }
+
+    public static String convertStringToDate(Date date)
+    {
+        DateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
+        return(df.format(date));
     }
 
 }
