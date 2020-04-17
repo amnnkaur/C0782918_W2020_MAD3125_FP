e32853783f53b5423b6a5fc6ac16433037770ce6
Amanpreet Kaur <amankaur2628@gmail.com>
2020-04-17 20:15:30 -0300

    [Aman]Code updated

--- app/src/main/java/com/example/c0782918_w2020_mad3125_fp/fragments/CusHydro.java
+++ app/src/main/java/com/example/c0782918_w2020_mad3125_fp/fragments/CusHydro.java
@@ -1,8 +1,6 @@
 package com.example.c0782918_w2020_mad3125_fp.fragments;
-
 import android.os.Build;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
@@ -10,13 +8,11 @@ import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
-
 import com.example.c0782918_w2020_mad3125_fp.R;
 import com.example.c0782918_w2020_mad3125_fp.adapter.HydroAdapter;
 import com.example.c0782918_w2020_mad3125_fp.databinding.FragmentCusHydroBinding;
@@ -24,19 +20,14 @@ import com.example.c0782918_w2020_mad3125_fp.model.Hydro;
 import com.example.c0782918_w2020_mad3125_fp.singleton.DataStorage;
 import com.example.c0782918_w2020_mad3125_fp.ui.CustomerListActivity;
 import com.example.c0782918_w2020_mad3125_fp.ui.ShowBillDetailsActivity;
-
 import java.util.ArrayList;
 import java.util.zip.Inflater;
-
-
+import static androidx.recyclerview.widget.LinearLayoutManager.*;
 public class CusHydro extends Fragment {
-
-    View view;
     private HydroAdapter hydroAdapter;
     private ArrayList<Hydro> hydroArrayList;
-
     FragmentCusHydroBinding cusHydroBinding;
-
+    private RecyclerView hydroRV;
     public CusHydro() {
         // Required empty public constructor
     }
@@ -45,25 +36,33 @@ public class CusHydro extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        view = inflater.inflate(R.layout.fragment_cus_hydro,container,false);
-        Toast.makeText(getContext(),"HydroFrag",Toast.LENGTH_SHORT).show();
-        hydroAdapter = new HydroAdapter(hydroArrayList);
-        cusHydroBinding.rvCusHydro.setLayoutManager(new LinearLayoutManager(getActivity()));
-        cusHydroBinding.rvCusHydro.setAdapter(hydroAdapter);
-        DataStorage.getInstance().loadData();
-        populateHydroBill();
+        View view = inflater.inflate(R.layout.fragment_cus_hydro,container,false);
         return view ;
     }
-
-
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ShowBillDetailsActivity fragObject = (ShowBillDetailsActivity) getActivity();
+//        fragObject.toastString = "Activity: From Red Fragment";
+        hydroRV = view.findViewById(R.id.rvCusHydro);
+//        hydroAdapter = new HydroAdapter(hydroArrayList);
+//        cusHydroBinding.rvCusHydro.setLayoutManager(new LinearLayoutManager(getActivity()));
+//        cusHydroBinding.rvCusHydro.setAdapter(hydroAdapter);
+        DataStorage.getInstance().loadData();
+        populateHydroBill();
+    }
     public void populateHydroBill(){
+//        Toast.makeText(getContext(),"HydroFrag",Toast.LENGTH_SHORT).show();
         hydroArrayList = new ArrayList<>();
-
-        if(DataStorage.getInstance().getAllCustomers().size() == 0){
-            Toast.makeText(getContext(), "Null", Toast.LENGTH_SHORT).show();
+        if(DataStorage.getInstance().getAllHydro().size() == 0){
+            Toast.makeText(getContext(), String.valueOf(DataStorage.getInstance().getAllHydro().size()), Toast.LENGTH_SHORT).show();
         }else{
-            hydroArrayList = DataStorage.getInstance().getAllHydro();}
-
-
+            Toast.makeText(getContext(),String.valueOf(DataStorage.getInstance().getAllHydro().get(2).getBillID()),Toast.LENGTH_SHORT).show();
+            hydroArrayList = DataStorage.getInstance().getAllHydro();
+            hydroAdapter = new HydroAdapter(hydroArrayList);
+//            hydroRV.setLayoutManager();
+            hydroRV.setAdapter(hydroAdapter);
+        }
     }
 }
--- app/src/main/java/com/example/c0782918_w2020_mad3125_fp/ui/ShowBillDetailsActivity.java
+++ app/src/main/java/com/example/c0782918_w2020_mad3125_fp/ui/ShowBillDetailsActivity.java
@@ -1,5 +1,4 @@
 package com.example.c0782918_w2020_mad3125_fp.ui;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
@@ -7,11 +6,9 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.viewpager.widget.ViewPager;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
-
 import com.example.c0782918_w2020_mad3125_fp.R;
 import com.example.c0782918_w2020_mad3125_fp.adapter.HydroAdapter;
 import com.example.c0782918_w2020_mad3125_fp.adapter.InternetAdapter;
@@ -27,115 +24,79 @@ import com.example.c0782918_w2020_mad3125_fp.model.Internet;
 import com.example.c0782918_w2020_mad3125_fp.model.Mobile;
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
-
 import java.util.ArrayList;
-
 public class ShowBillDetailsActivity extends AppCompatActivity {
-
     ActivityShowBillDetailsBinding showBillDetailsBinding;
-
     /*private HydroAdapter hydroAdapter;
     private InternetAdapter internetAdapter;
     private MobileAdapter mobileAdapter;*/
     public PagerAdapter pagerAdapter;
-
+    FragmentManager mFragmentManager;
+    FragmentTransaction mFragmentTransaction;
     /*private ArrayList<Hydro> hydroArrayList;
     private ArrayList<Mobile> mobileArrayList;
     private ArrayList<Internet> internetArrayList;*/
-
     Bundle fetch;
     Customer object;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         showBillDetailsBinding = DataBindingUtil.setContentView(this, R.layout.activity_show_bill_details);
-
         Intent myIntent = getIntent();
         fetch = myIntent.getBundleExtra("object");
         object = (Customer) fetch.getSerializable("customerDetail");
         showBillDetailsBinding.setCustomer(object);
-
        /* hydroAdapter = new HydroAdapter(hydroArrayList);
         internetAdapter = new InternetAdapter(internetArrayList);
         mobileAdapter = new MobileAdapter(mobileArrayList);
 */
-
         /*setupViewPager(showBillDetailsBinding.vpBills);
         showBillDetailsBinding.vpBills.setAdapter(pagerAdapter);
         showBillDetailsBinding.tabCusBills.setupWithViewPager(showBillDetailsBinding.vpBills);
 */
-        FragmentManager mFragmentManager = getSupportFragmentManager();
-        CusHydro cusHydro = new CusHydro();
-        FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
-        mFragmentTransaction.add(R.id.fragment_2, cusHydro, "HYDRO");
+        mFragmentManager = getSupportFragmentManager();
+        mFragmentTransaction = mFragmentManager.beginTransaction();
+        mFragmentTransaction.add(R.id.fragment_2, new CusHydro());
         mFragmentTransaction.commit();
-
-       showBillDetailsBinding.tabCusBills.addTab(showBillDetailsBinding.tabCusBills.newTab().setText("HYDRO"),true);
-       showBillDetailsBinding.tabCusBills.addTab(showBillDetailsBinding.tabCusBills.newTab().setText("INTERNET"));
-       showBillDetailsBinding.tabCusBills.addTab(showBillDetailsBinding.tabCusBills.newTab().setText("MOBILE"));
-
-       showBillDetailsBinding.tabCusBills.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+        Fragment fragValue = null;
+        fragValue = new CusHydro();
+        mFragmentTransaction.replace(R.id.fragment_2, fragValue);
+//       showBillDetailsBinding.tabCusBills.addTab(showBillDetailsBinding.tabCusBills.newTab().setText("HYDRO"),true);
+//       showBillDetailsBinding.tabCusBills.addTab(showBillDetailsBinding.tabCusBills.newTab().setText("INTERNET"));
+//       showBillDetailsBinding.tabCusBills.addTab(showBillDetailsBinding.tabCusBills.newTab().setText("MOBILE"));
+        showBillDetailsBinding.tabCusBills.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-
-                FragmentManager mFragmentManager = getSupportFragmentManager();
-                FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
-
-                showBillDetailsBinding.vpBills.setCurrentItem(tab.getPosition());
-                if(tab.getPosition() == 0){
-                    CusHydro cusHydro = new CusHydro();
-                    Toast.makeText(ShowBillDetailsActivity.this, "Hydro", Toast.LENGTH_SHORT).show();
-                    getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.fragment_2, cusHydro).commit();
-                   /* pagerAdapter.AddFragment(cusHydro,"HYDRO");
-                    pagerAdapter.notifyDataSetChanged();*/
-
-                }
-                else if(tab.getPosition() == 1){
-                    CusInternet cusInternet = new CusInternet();
-                    Toast.makeText(ShowBillDetailsActivity.this, "Internet", Toast.LENGTH_SHORT).show();
-                    getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.fragment_2, cusInternet).commit();
-                   /* pagerAdapter.AddFragment(new CusInternet(),"INTERNET");
-                    pagerAdapter.notifyDataSetChanged();
-                    mFragmentTransaction.replace(R.id.fragment2, cusInternet);*/
-                }
-                else if(tab.getPosition() == 2){
-                    CusMobile cusMobile = new CusMobile();
-                    Toast.makeText(ShowBillDetailsActivity.this, "Mobile", Toast.LENGTH_SHORT).show();
-                    getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.fragment_2, cusMobile).commit();
-                    /*pagerAdapter.AddFragment(new CusMobile(),"MOBILE");
-                    pagerAdapter.notifyDataSetChanged();
-                    mFragmentTransaction.replace(R.id.fragment2, cusMobile);*/
-
+                mFragmentTransaction = mFragmentManager.beginTransaction();
+                Fragment fragValue = null;
+                switch(tab.getPosition()){
+                    case 0:
+                        fragValue = new CusHydro();
+                        mFragmentTransaction.replace(R.id.fragment_2, fragValue);
+                        break;
+                    case 1:
+                        fragValue = new CusHydro();
+                        mFragmentTransaction.replace(R.id.fragment_2, fragValue);
+                        break;
+                    case 2:
+                        fragValue = new CusHydro();
+                        mFragmentTransaction.replace(R.id.fragment_2, fragValue);
+                        break;
                 }
                 mFragmentTransaction.commit();
             }
-
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
-
             }
-
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
-
             }
         });
-
-
-        //showBillDetailsBinding.vpBills.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(showBillDetailsBinding.tabCusBills));
-    }
-
     /*private void setupViewPager(ViewPager viewPager) {
         PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager());
         adapter.AddFragment(new CusHydro(), "HYDRO");
         adapter.AddFragment(new CusInternet(),"INTERNET");
         adapter.AddFragment(new CusMobile(),"MOBILE");
-        viewPager.setAdapter(adapter);
+        viewPager.setAdapter(adapter);*/
     }
-*/
-
 }
