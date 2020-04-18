a8758be5c0702ffd334d434f187775f154e620c2
Amanpreet Kaur <amankaur2628@gmail.com>
2020-04-17 21:04:11 -0300

    [Aman]HydAdapter added

---
+++ app/src/main/java/com/example/c0782918_w2020_mad3125_fp/adapter/HydAdapter.java
@@ -0,0 +1,74 @@
+package com.example.c0782918_w2020_mad3125_fp.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.c0782918_w2020_mad3125_fp.R;
+import com.example.c0782918_w2020_mad3125_fp.model.Hydro;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+
+public class HydAdapter extends RecyclerView.Adapter<HydAdapter.HydroViewHolder> {
+
+    private ArrayList<Hydro> hydroArrayList;
+    NumberFormat defaultFormat = NumberFormat.getCurrencyInstance();
+
+    public HydAdapter(ArrayList<Hydro> hydroArrayList) {
+        this.hydroArrayList = hydroArrayList;
+    }
+
+    @NonNull
+    @Override
+    public HydAdapter.HydroViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View mView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_hydro,
+                parent,
+                false);
+        HydroViewHolder myHydroViewHolder = new HydroViewHolder(mView);
+
+        return myHydroViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HydroViewHolder holder, int position) {
+        Hydro hydroObject = this.hydroArrayList.get(position);
+        holder.id.setText(hydroObject.getBillID());
+        holder.agencyName.setText(hydroObject.getAgencyName());
+        holder.unitsConsumed.setText(hydroObject.getUnitsConsumed() + " Units");
+
+
+
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        return hydroArrayList.size();
+    }
+
+
+    public class HydroViewHolder extends RecyclerView.ViewHolder{
+
+        TextView id;
+
+        TextView agencyName;
+        TextView unitsConsumed;
+
+        public HydroViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            id = itemView.findViewById(R.id.txtHydBillID);
+
+            agencyName = itemView.findViewById(R.id.txtAgencyName);
+            unitsConsumed = itemView.findViewById(R.id.txtUnits);
+
+        }
+    }
+}
+
--- app/src/main/java/com/example/c0782918_w2020_mad3125_fp/fragments/CusHydro.java
+++ app/src/main/java/com/example/c0782918_w2020_mad3125_fp/fragments/CusHydro.java
@@ -37,6 +37,7 @@ public class CusHydro extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_cus_hydro,container,false);
+
         return view ;
     }
     @RequiresApi(api = Build.VERSION_CODES.O)
